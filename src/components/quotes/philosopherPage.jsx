e3a37332c319e9f0a6e9b05492de9057088387c9
eteen12 <eteensmash@example.com>
2024-10-25 10:35:06 -0700

    added a nested id page for each individual quote for quote sharing

--- src/components/quotes/philosopherPage.jsx
+++ src/components/quotes/philosopherPage.jsx
@@ -2,26 +2,26 @@ import Image from "next/image";
 
 export default function PhilosopherPage({ philosopher }) {
   return (
-    <div className="overflow-hidden whiteBg rounded-xl">
+    <div className="overflow-hidden whiteBg rounded-xl blackText tracking-tight">
       <div className="relative mx-auto max-w-7xl px-6 py-16 lg:px-8">
         <div className="absolute bottom-0 left-3/4 top-0 hidden w-screen bg-gray-50 lg:block" />
         <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
           <div>
-            <h2 className="text-lg font-semibold text-indigo-600">About</h2>
-            <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            <h2 className="text-lg">About</h2>
+            <h3 className="mt-2 text-3xl bold leading-8 sm:text-4xl">
               {philosopher.name} quotes, and biography
             </h3>
           </div>
         </div>
         <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
-          <div className="relative lg:col-start-2 lg:row-start-1">
-            <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
+          <div className="relative lg:col-start-2 lg:row-start-1 rounded-xl  lg:-mt-14">
+            <div className="relative mx-auto max-w-prose text-base lg:max-w-none ">
               <Image
                 alt={philosopher.name}
                 src={philosopher.image}
                 width={1184}
                 height={1376}
-                className="rounded-lg object-cover object-center shadow-lg"
+                className="rounded-lg object-cover object-center "
               />
             </div>
           </div>
@@ -29,7 +29,7 @@ export default function PhilosopherPage({ philosopher }) {
             <div className="mx-auto max-w-prose text-base lg:max-w-none">
               <p className="text-lg blackText">{philosopher.bio}</p>
             </div>
-            <ul className="about-list blackText">
+            <ul className="about-list blackText text-lg ">
               {philosopher.details.map((detail, index) => (
                 <li key={index} className="pt-2">
                   <strong>{detail.label}:</strong> {detail.text}
--- src/components/quotes/philosopherQuotes.jsx
+++ src/components/quotes/philosopherQuotes.jsx
@@ -1,24 +1,34 @@
 import { FaShareFromSquare } from "react-icons/fa6";
+import Link from "next/link";
 
 function PhilosopherQuotes({ thisPhilosophersQuotes }) {
   return (
-    <div className="quote-container blackText tracking-tight flex justify-center p-5">
-      <ul role="list" className="divide-y divide-gray-200 max-w-[700px]">
-        {thisPhilosophersQuotes.quotes.map((quote) => (
-          <li key={quote.index} className="py-10">
-            <blockquote className="quote text-lg lg:text-xl">
-              {quote.text}
-            </blockquote>
-            <div className="flex gap-10">
-              <p className="text-lg lg:text-xl">
-                - <strong>{thisPhilosophersQuotes.name}</strong>
-              </p>
-              <FaShareFromSquare className="mt-1 text-2xl" />
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h2 className="flex justify-center text-center blackText text-3xl md:text-4xl tracking-tight bold mt-10">
+        Famous Quotes
+      </h2>
+      <div className="quote-container blackText tracking-tight flex justify-center p-5">
+        <ul role="list" className="divide-y divide-gray-200 max-w-[700px]">
+          {thisPhilosophersQuotes.quotes.map((quote) => (
+            <li key={quote.index} className="py-10">
+              <blockquote className="quote text-lg lg:text-xl">
+                {quote.text}
+              </blockquote>
+              <div className="flex gap-10">
+                <p className="text-lg lg:text-xl">
+                  - <strong>{thisPhilosophersQuotes.name}</strong>
+                </p>
+                <Link
+                  href={`/quotes/${thisPhilosophersQuotes.name.toLowerCase()}/${quote.index}`}
+                >
+                  <FaShareFromSquare className="mt-1 text-2xl" />
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
