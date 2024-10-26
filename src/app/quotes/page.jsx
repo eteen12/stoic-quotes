a20f37e90841c82c37306e33cde7105295877212
eteen12 <eteensmash@example.com>
2024-10-25 21:51:07 -0700

    improved seo and accessibility in quotes

--- src/app/quotes/page.jsx
+++ src/app/quotes/page.jsx
@@ -1,15 +1,93 @@
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
+
+export const metadata = {
+  title:
+    "Inspiring Stoic Quotes: Wisdom from Marcus Aurelius, Seneca, and Epictetus",
+  description:
+    "Explore profound insights and motivational quotes from Stoic philosophers Marcus Aurelius, Seneca, and Epictetus. Enhance your resilience and wisdom with timeless teachings.",
+  keywords:
+    "Stoic philosophy, inspirational quotes, Marcus Aurelius, Seneca, Epictetus, wisdom, resilience, personal development, mental strength, daily motivation",
+  author: "Teleta Development",
+  og: {
+    title: "Inspiring Stoic Quotes",
+    description:
+      "Explore profound insights and motivational quotes from Stoic philosophers Marcus Aurelius, Seneca, and Epictetus. Enhance your resilience and wisdom with timeless teachings.",
+    url: "https://www.stoicquotes.ca/quotes",
+    image: "https://www.stoicquotes.ca/bento/marcusFaceLogo.webp",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Inspiring Stoic Quotes",
+    description:
+      "Explore profound insights and motivational quotes from Stoic philosophers Marcus Aurelius, Seneca, and Epictetus. Enhance your resilience and wisdom with timeless teachings.",
+    image: "https://www.stoicquotes.ca/bento/marcusFaceLogo.webp",
+  },
+  schema: {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "Inspiring Stoic Quotes",
+    url: "https://www.stoicquotes.ca/quotes",
+    description:
+      "Explore profound insights and motivational quotes from Stoic philosophers Marcus Aurelius, Seneca, and Epictetus. Enhance your resilience and wisdom with timeless teachings.",
+    image: "https://www.stoicquotes.ca/bento/marcusFaceLogo.webp",
+    sameAs: [
+      "https://www.facebook.com/TeletaDevelopment",
+      "https://www.instagram.com/Teleta_Development/",
+      "https://www.linkedin.com/in/ethan-breitkreutz-43399b322/",
+    ],
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://www.stoicquotes.ca/quotes/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+  },
+};
+
 function Page() {
   return (
     <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+
+        {/* Open Graph tags */}
+        <meta property="og:title" content={metadata.og.title} />
+        <meta property="og:description" content={metadata.og.description} />
+        <meta property="og:url" content={metadata.og.url} />
+        <meta property="og:image" content={metadata.og.image} />
+        <meta property="og:type" content={metadata.og.type} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta
+          name="twitter:description"
+          content={metadata.twitter.description}
+        />
+        <meta name="twitter:image" content={metadata.twitter.image} />
+
+        {/* Schema markup */}
+        <script type="application/ld+json">
+          {JSON.stringify(metadata.schema)}
+        </script>
+      </Head>
+
       <div className="pb-20">
         <div className="relative isolate px-6 lg:px-8 ">
           <div className="mx-auto max-w-5xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 transition-transform duration-300 transform hover:scale-105">
                 read about the developer and writer{" "}
-                <Link href="/about" className="font-semibold blackText">
+                <Link
+                  href="/about"
+                  className="font-semibold blackText"
+                  aria-label="read about ethan, the stoic quotes developer"
+                >
                   <span aria-hidden="true" className="absolute inset-0" />
                   read more <span aria-hidden="true">&rarr;</span>
                 </Link>
@@ -33,7 +111,7 @@ function Page() {
           <div className="relative h-full w-full rounded-xl">
             <Image
               src="/quotes/marcusOnHorse.webp"
-              alt="hi"
+              alt="Marcus Aurelius equestrian statue black and white image"
               fill
               className="object-cover rounded-xl"
               loading="lazy"
@@ -50,6 +128,7 @@ function Page() {
             </p>
             <Link
               href="/philosophers/marcus-aurelius"
+              aria-label="read marcus aurelius quotes"
               className="rounded-md mt-10 blackBg px-7 py-1.5 whiteText shadow-sm hoverBg  tracking-tighter bold text-base absolute bottom-0 mb-10"
             >
               read quotes
@@ -61,7 +140,7 @@ function Page() {
           <div className="relative h-full w-full rounded-xl">
             <Image
               src="/quotes/senecaDeath.webp"
-              alt="hi"
+              alt="seneca's suicide, forced by nero painting black and white"
               fill
               className="object-cover rounded-xl"
               loading="lazy"
@@ -79,6 +158,7 @@ function Page() {
             </p>
             <Link
               href="/philosophers/seneca"
+              aria-label="read seneca quotes"
               className="rounded-md mt-10 blackBg px-7 py-1.5 whiteText shadow-sm hoverBg  tracking-tighter bold text-base absolute bottom-0 mb-10"
             >
               read quotes
@@ -92,7 +172,7 @@ function Page() {
           <div className="relative h-full w-full rounded-xl">
             <Image
               src="/bento/epictetusWriting.webp"
-              alt="hi"
+              alt="epictetus writing black and white image"
               fill
               className="object-cover rounded-xl"
               loading="lazy"
@@ -110,6 +190,7 @@ function Page() {
             </p>
             <Link
               href="/philosophers/epictetus"
+              aria-label="read epictetus quotes"
               className="rounded-md mt-10 blackBg px-7 py-1.5 whiteText shadow-sm hoverBg  tracking-tighter bold text-base absolute bottom-0 mb-10"
             >
               read quotes
