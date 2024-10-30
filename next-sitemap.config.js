5794fe8c622c8be48ab3f21dbb06797080e34764
eteen12 <eteensmash@gmail.com>
2024-10-30 10:49:29 -0700

    changed some sitemap info, and metadata

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://www.stoicquotes.ca",
-  generateRobotsTxt: true,
-  defaultChangeFreq: 'weekly',
-};
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: process.env.SITE_URL || "https://www.stoicquotes.ca",
+//   generateRobotsTxt: true,
+//   defaultChangeFreq: "weekly",
+// };
