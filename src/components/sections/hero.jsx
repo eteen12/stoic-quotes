ba1f5a064c04d7a6d7f37771f8cdb22fbf9cc6aa
eteen12 <eteensmash@gmail.com>
2024-11-10 13:51:43 -0800

    medium hero section completed

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -6,8 +6,8 @@ export default function Hero() {
   return (
     <div className=" h-fit py-24 sm:pt-32 px-4 sm:px-8 transition-all duration-700">
       <div className="blackText w-full pt-10 flex">
-        <h1 className="blackText text-6xl sm:text-8xl">Stoicism</h1>
-        <span className="aria-hidden h-1.5 w-10 blackBg mt-8 ml-2 sm:mt-14 sm:h-2 sm:w-12"></span>
+        <h1 className="blackText text-6xl sm:text-8xl md:text-9xl">Stoicism</h1>
+        <span className="aria-hidden h-1.5 w-10 blackBg mt-8 ml-2 sm:mt-14 sm:h-2 sm:w-12 md:mt-20 md:h-3 md:w-14"></span>
       </div>
       <div className="flex gap-1 mt-5 sm:mt-8">
         <Button
@@ -21,14 +21,14 @@ export default function Hero() {
           className="py-0.5 text-xs sm:text-sm"
         />
       </div>
-      <div className="w-full mt-16 text-lg sm:text-xl sm:mt-24">
+      <div className="w-full mt-16 text-lg sm:text-xl sm:mt-24 md:w-[65%] md:ml-auto">
         <h2>
           Read quotes from the famous stoics, and read the daily quote. The blog
           is updated weekly with insights on stoic applications to modern day
           life.
         </h2>
         <div className="relative w-full h-full mt-8">
-          <div className="relative w-full h-[350px] sm:h-[420px]">
+          <div className="relative w-full h-[350px] sm:h-[420px] md:h-[500px]">
             <SrcSetImage
               sizes="(max-width: 1000px) 100vw, 1000px"
               srcSet="
