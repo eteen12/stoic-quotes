f5e2cbf5c17aa0c327923331218d6039f2de4fb7
eteen12 <eteensmash@gmail.com>
2024-11-10 21:08:12 -0800

    desktop navbar completed

--- src/components/layout/navBar.jsx
+++ src/components/layout/navBar.jsx
@@ -8,6 +8,8 @@ import NavBurger from "./navBurger";
 const links = [
   { href: "/quotes", text: "Quotes" },
   { href: "/blog", text: "Blog" },
+  { href: "/about", text: "About" },
+  { href: "/contact", text: "Contact" },
 ];
 const menuItems = [
   { name: "About", path: "/about" },
@@ -23,12 +25,15 @@ export default function NavBar() {
     <header
       className={`fixed justify-between items-center w-full h-14 z-50 transition-all duration-700 blackText tracking-tight text-2xl
     ${scrollingDown ? "-translate-y-full" : "translate-y-0"}`}
-      style={{ boxShadow: shadow, background: `rgba(255, 255, 255, ${opacity})` }}
+      style={{
+        boxShadow: shadow,
+        background: `rgba(255, 255, 255, ${opacity})`,
+      }}
     >
       <div className="flex justify-between items-center w-full px-4 sm:px-8  mt-2.5 mx-auto max-w-[1700px]">
         <Link href="/" aria-label="Go to stoic quotes home page">
           <h2 className="relative tracking-tight shrinkAnimation transition3 hoverText">
-            StoicQuotes.ca
+            Stoicquotes.ca
           </h2>
         </Link>
 
@@ -45,46 +50,7 @@ export default function NavBar() {
                 </Link>
               </li>
             ))}
-            <li className="ml-10 text-xl md:text-2xl">
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <MenuButton
-                    className="rounded-md blackBg px-8 py-1.5 whiteText shadow-sm hoverBg flex tracking-tight md:text-2xl -mr-2 transition-transform duration-300 transform hover:scale-105"
-                    aria-label="click the more button for an extended choice of pages"
-                  >
-                    More
-                  </MenuButton>
-                </div>
-                <MenuItems
-                  transition
-                  className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                >
-                  <div className="py-1">
-                    {menuItems.map((item, index) => (
-                      <MenuItem key={index}>
-                        {item.isButton ? (
-                          <button
-                            type="submit"
-                            aria-label={`go to the ${item.name} page`}
-                            className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                          >
-                            {item.name}
-                          </button>
-                        ) : (
-                          <Link
-                            href={item.path}
-                            aria-label={`go to the ${item.name} page`}
-                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                          >
-                            {item.name}
-                          </Link>
-                        )}
-                      </MenuItem>
-                    ))}
-                  </div>
-                </MenuItems>
-              </Menu>
-            </li>
+            <li className="text-xl md:text-2xl"></li>
           </ul>
         </div>
 
